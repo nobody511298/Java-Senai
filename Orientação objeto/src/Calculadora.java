644de5ea4d2d651616422f4878bdaea7ad7a9e9a
aluno <aluno@DSN1251017050>
2022-04-04 10:34:06 -0300

    commit ok

---
+++ Orientação objeto/src/Calculadora.java
@@ -0,0 +1,23 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     double n1 = 5;
+     double n2 = 2;
+     double resulS;
+     double resulSU;
+     double resulM;
+     double resulD;
+     
+     resulS = n1 + n2;
+     System.out.println("O resultado da soma é de " + resulS);
+     resulSU = n1 - n2;
+     System.out.println("O resultado da subtração é de " + resulSU);
+     resulM = n1 * n2;
+     System.out.println("O resultado da multiplicação é de " + resulM);
+     resulD = n1 / n2;
+     System.out.println("O resutlado da divisão é de " + resulD);
+	}
+
+}
