bfe2dd9a6d38db3ac8ce56a60c8c3afd8b303c19
aluno <aluno@DSN1251017050>
2022-04-04 11:16:37 -0300

    PessoaJava

---
+++ Orientacaoeoobjeto/src/Pessoa.java
@@ -0,0 +1,12 @@
+
+public class Pessoa {
+String nome;
+int numFig;
+
+void receber(int numFig) {
+	this.numFig = numFig;
+}
+
+void dar( )
+}
+}
---
+++ Orientacaoeoobjeto/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Pessoa pessoa1 = new Pessoa();
+
+	}
+
+}
