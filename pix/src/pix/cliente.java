d16ed3221644dfb04a45fa0d879636396d7de8e6
aluno <aluno@DSN1251017050>
2022-04-11 11:20:12 -0300

    att 2

---
+++ pix/src/pix/cliente.java
@@ -0,0 +1,40 @@
+package pix;
+
+import java.util.Scanner;
+
+public class cliente {
+	
+	
+
+	public static void main(String[] args) {
+		String cliente;
+		int conta;
+		pixclass pix;
+		float saldo;
+		int n;
+		
+		
+		Scanner entrada = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		
+		System.out.println("Digite seu nome");
+		cliente = entrada.nextLine();
+		System.out.println(cliente);
+		
+		System.out.println("Digite sua conta");
+		conta = entrada.nextLine();
+		System.out.println(cliente);
+		
+		System.out.println("Digite pix");
+		pix = entrada.nextLine();
+		System.out.println(cliente);
+   
+	
+   
+	
+   
+   
+    
+	}
+
+}
---
+++ pix/src/pix/pixclass.java
@@ -0,0 +1,14 @@
+package pix;
+
+public class pixclass {
+	int chcpf;
+	int chtel;
+	String chal;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
