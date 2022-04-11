b7200a7f522d529381ff5195c48f638f640b5c9d
aluno <aluno@DSN1251017050>
2022-04-11 10:00:08 -0300

    teste do pix

--- Orientacaoeoobjeto/src/main.java
+++ Orientacaoeoobjeto/src/main.java
@@ -1,9 +1,16 @@
+package Orientacaoeoobjeto
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Pessoa pessoa1 = new Pessoa();
+           Pessoa pessoa1 = new Pessoa();
+           pessoa1.nome = "Adrian";
+           pessoa1.numFig = 1;
+           
+System.out.println(pessoa1.nome);
+System.out.println(pessoa1.numFig);
+
 
 	}
 
