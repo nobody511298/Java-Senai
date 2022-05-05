907caeb4466f75ac038485b388e7ba47637ed4be
aluno <aluno@DSN1251017050>
2022-05-05 11:25:36 -0300

    commit 05/05/22

---
+++ Polimorfismo/src/Gato.java
@@ -0,0 +1,7 @@
+
+public class Gato extends Animal{
+	@Override
+	public void Falar() {
+		System.out.println("miau");
+	}
+}
---
+++ Polimorfismo/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Animal animal = new Cachorro();
+		animal.Falar();
+		
+		animal = new Gato();
+		animal.Falar();
+		
+	}
+	}
