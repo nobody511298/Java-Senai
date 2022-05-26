fc87362778bcb3b7b2754ce02acf9f37ad39a1f1
aluno <aluno@10.107.71.36>
2022-05-26 10:08:22 -0300

    26/05

---
+++ LerTxT/src/main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		try(BufferedReader reader = new BufferedReader(new FileReader("Entrada.txt"))){
+			String line;
+			String string = "";
+			
+			while((line = reader.readLine())!=null) {
+				string += line + "\n";
+			
+			}
+			System.out.println(string);
+		}
+
+	}
+
+}
